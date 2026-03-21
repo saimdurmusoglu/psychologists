ab15b19bf9e2721c65a12f4754603731d31325e0
Saim Durmuşoğlu <saim.durmusoglu@gmail.com>
2026-03-21 16:11:11 +0300

    21.03.2026 16:11 firebase.ts düzeltmesi

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,7 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
-export default app```
-
-**`.gitignore`'a ekle:**
-```.env.env.local;
